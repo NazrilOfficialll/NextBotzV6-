ebfda2a6de92b8583dab796fe7cdd7ed85c64ba4
NazrilOfficialll <122782101+NazrilOfficialll@users.noreply.github.com>
2023-01-16 16:33:07 +0700

    Update payment.js

--- plugins/payment.js
+++ plugins/payment.js
@@ -12,12 +12,12 @@ let handler = async (m, { conn, usedPrefix }) => {
   let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Gopay  [082214729677]
-┃✾ Dana  [083105060798]
-┃✾ Saweria  [https://saweria.co/PutraModz]
+┃✾ Gopay  [081393160368]
+┃✾ Dana  [-]
+┃✾ Saweria  [-]
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/6282214729677
+┃ Ingin donasi? Wa.me/6281393160368
 ┃ _Hasil donasi akan digunakan buat sewa_
 ┃ _atau beli *RDP/VPS* agar bot bisa jalan_
 ┃ _24jam tanpa kendala_
