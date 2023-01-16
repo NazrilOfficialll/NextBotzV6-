3d917258e3b77e6e21d8c79a21bc74a3b9bebfeb
NazrilOfficialll <122782101+NazrilOfficialll@users.noreply.github.com>
2023-01-16 16:24:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,12 +27,12 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
       
 
 /*============== INFO OWNER ==============*/
-global.sig = 'https://instagram.com/l4c1f2r'
-global.syt = 'https://youtube.com/@Putra_Modz_'
-global.sgh = 'https://github.com/Lucife-Putra'
-global.sgc = 'https://chat.whatsapp.com/FR1KfE3OTcQCayiabos8Ff'
-global.sdc = 'https://wa.me/6282214729677'
-global.swa = 'wa.me/682214729677'
+global.sig = 'https://instagram.com/Nazril36'
+global.syt = 'https://youtube.com/'
+global.sgh = 'https://github.com/NazrilOfficiall l'
+global.sgc = 'https://chat.whatsapp.com/KeA2YNkBvddIp70ReuqfKF'
+global.sdc = 'https://wa.me/6281393160368'
+global.swa = 'wa.me/681393160368'
 global.swb = 'https://discord.gg/'
 global.snh = 'https://nhentai.net/g/365296/' //Make ini aja gausah di ganti.
 
@@ -45,18 +45,18 @@ global.ppulsa2 = '-'
 global.psaweria = '-'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '6283863727413'
-global.nomorown = '6282214729677'
-global.namebot = 'NexBotz'
-global.nameown = 'PutraModz'
+global.nomorbot = '6285228129591'
+global.nomorown = '6281393160368'
+global.namebot = '𝙉𝙖𝙯𝙧𝙞𝙡𝙈𝘿'
+global.nameown = '𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282214729677'],
-  ['6283863727733'],
-  ['62838637277192'],
-  ['6282214729677', 'PutraModz', true]
+  ['6281393160368'],
+  ['6281393160368'],
+  ['6281393160368'],
+  ['6281393160368', '𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -108,22 +108,22 @@ global.bear = 'VBkM7rbU'
 
 
 /*============== WATERMARK ==============*/
-global.gcname = '𝑺𝒊𝒎𝒑𝒍𝒆 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑 𝑩𝒚 𝘼𝙗𝙞𝘿𝙚𝙫'//nama group kalian
-global.wm = 'By NexBotz' //Main Watermark
-global.wm2 = 'PutraModz'
-global.wm3 = 'NexBotz | By PutraModz'
+global.gcname = '𝑺𝒊𝒎𝒑𝒍𝒆 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑 𝑩𝒚 𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡'//nama group kalian
+global.wm = 'By NazrilMD' //Main Watermark
+global.wm2 = 'NazrilOffc'
+global.wm3 = 'NazrilMD | By NazrilOffcz'
 global.botdate = `⻝ 𝗗𝗮𝘁𝗲: ${week} ${date}\n⻝ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `⻝ 𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
-global.titlebot = `NexBotz | ʙʏ ${global.nameown}`
-global.author = 'PutraModz' 
-global.packname = 'PutraModz'
+global.titlebot = `𝐍𝐚𝐳𝐫𝐢𝐥𝐌𝐃 | ʙʏ ${global.nameown}`
+global.author = '𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡' 
+global.packname = '𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡'
 global.danied = '✘ 𝗘𝗥𝗢𝗥 𝟰𝟬𝟰'
-global.ultahowner = 'Februari 02 2023'
+global.ultahowner = 'desember 24 2023'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://telegra.ph/file/4149491a34cdd06cd6dd7.jpg' //Main Thumbnail
-global.imagebot = 'https://telegra.ph/file/06c4e93a3d3f388b2642d.jpg'
+global.thumb = 'https://telegra.ph/file/c1aa945f8cf68e488df90.jpg' //Main Thumbnail
+global.imagebot = 'https://telegra.ph/file/c1aa945f8cf68e488df90.jpg'
 global.giflogo = 'https://ibb.co/3sJ0ybv'
 global.thumbbc = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4' //For broadcast
 global.giflogo = 'https://telegra.ph/file/e220e49e421810bbdef6a.mp4'
@@ -132,14 +132,14 @@ global.thumbs = ['https://telegra.ph/file/7d80f81dd7c3ed1ff85a1.jpg']
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.thumbBanned = 'https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg'
 global.thumbWarn = 'https://telegra.ph/file/a5eb5169ab1330a537e0b.jpg'
-global.botintro = 'Hallo, Kenalin saya NexBotz Saya akan membantu Anda kapan pun & Dimana pun, Saya asisten bot WhatsApp yang di buat oleh NexEp Saya di rilis pada tanggal\n🗓 ️_16/10/2022_'
-global.intro = 'https://telegra.ph/file/6ab4daac226292a112540.jpg'
+global.botintro = 'Hallo, Kenalin saya NazrilBotz Saya akan membantu Anda kapan pun & Dimana pun, Saya asisten bot WhatsApp yang di buat oleh NexEp Saya di rilis pada tanggal\n🗓 ️_16/10/2022_'
+global.intro = 'https://telegra.ph/file/c1aa945f8cf68e488df90.jpg'
 /*============== GLOBAL BIO ==============*/
-global.nameowner = 'PutraModz'
+global.nameowner = '𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡'
 global.umurowner = '18'
-global.asalowner = 'Jawa Barat, Kuningan'
-global.nomorowner = 'wa.me/6282214729677'
-global.pacarowner = 'Amelia Putri septiany'
+global.asalowner = 'Jawa Tengah, Purbalingga'
+global.nomorowner = 'wa.me/6281393160368'
+global.pacarowner = 'Desi'
 
 /*============== TEXT ==============*/
 global.wait = '```𝙇𝙤𝙖𝙙𝙞𝙣𝙜...```'
@@ -387,8 +387,8 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/fd/21/41/fd21419275236bb153de3c8dcbbf3bf9.jpg',
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
-global.stickpack = 'By NexBotz'
-global.stickauth = `PutraModz`
+global.stickpack = 'By NazrilBotz'
+global.stickauth = `𝙉𝙖𝙯𝙧𝙞𝙡𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡𝙡`
 
 global.multiplier = 38 // The higher, The harder levelup
 
